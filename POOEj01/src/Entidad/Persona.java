d8885c6a418a2b7573ff9702fa178d76da4a44cb
“Sofia <sofiaperezblanes@gmail.com>
2023-03-28 11:20:10 -0300

    Guia POO en proceso

---
+++ POOEj01/src/Entidad/Persona.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entidad;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Sofi
+ */
+public class Persona {
+    public String nombre;
+    public Date fechaNacimiento;
+    public String estadoCivil;
+    
+}
